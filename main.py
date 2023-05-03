0892e4fc8b19f79606b7c70abd0967227a4dff79
Eyad Saeed <107518595+eyadjs@users.noreply.github.com>
2023-05-03 13:18:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-from formats import transform
+from formats import transform, makeTable
 from parantheses import valid_parantheses
-from calculators import regular_calculator
+
 
 def compute():
 
     x = input('Enter your logical statement: ')
-    eq = transform(x) #eg. ([['T', 'AND', 'T', ')'], ... , ... , ...], ['p', 'and', 'q', ')'])
+    eq = transform(x) #eg. ([ ['T', 'AND', 'T', ')'], ... , ... , ...],
+                            # ['p', 'and', 'q', ')'],
+                            # ['p','q'] ])
 
+    isParantheses = False 
+    # checking if parantheses handling is needed
+    for i in eq[1]:
+        if i in (')','('):
+            isParantheses = True
+
+
+    # error handling
+    if len(eq[2]) not in (2,3):
+        return 'Please use 2 or 3 variables.'
     if valid_parantheses(eq[1]) == False:
-        return 'Fix your parantheses!'
+        return 'Fix your parantheses.'
     
-    return regular_calculator(eq[0])
+    
+    if isParantheses == True:
+        return 'Parantheses handling coming soon.'
+    else:
+        return makeTable(x)
 
     
 print(compute())
